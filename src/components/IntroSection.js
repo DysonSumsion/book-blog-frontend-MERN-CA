8ea038fe9f66d09de4f47f47c33596977b078470
Garry <pathakg028@gmail.com>
2019-07-22 15:52:16 +1000

    new final-query

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -11,6 +11,9 @@ function IntroSection(props) {
   <div className="IntroSection">
     <h1>{props.headingOne}</h1>
     <h2>{props.headingTwo}</h2>
+    <h2>{props.headingThree}</h2>
+    <img src={props.imgURL} alt="Image not found"/>
+    <h2>{props.headingFive}</h2>
   </div>
   );
 }
