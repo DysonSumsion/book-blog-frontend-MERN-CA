4eb61cf2326b7b9d17ae6ef876a555442c3ee683
Garry <pathakg028@gmail.com>
2019-07-24 12:41:34 +1000

    subscribe form validation

--- src/container/Subscribe.js
+++ src/container/Subscribe.js
@@ -8,7 +8,10 @@ class Subscribe extends React.Component {
   state = {
       firstName: "",
       lastName: "",
-      email: ""
+      email: "",
+      invalidEmail:"",
+      invalidFirstName:"",
+      invalidLastName:""
   }
 
   handleInput = e => {
@@ -19,10 +22,13 @@ class Subscribe extends React.Component {
     let value = e.target.value;
     if(source === 'firstName') {
       this.setState( {firstName : value});
+      this.setState({invalidFirstName:""});
     } else if (source === 'lastName') {
       this.setState( {lastName : value});
+      this.setState({invalidLastName:""});
     } else if (source === 'email') {
       this.setState( {email : value});
+      this.setState({invalidEmail:""});
     }
   };
 
@@ -34,10 +40,28 @@ class Subscribe extends React.Component {
         confirmEmail: ""
     });
   }
+  validateInput = () => {
+    const re1 = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    let emailValidity = re1.test(this.state.email);
+    if(!emailValidity) {
+      this.setState({invalidEmail:"Enter a valid email address"});
+    }
+    const re2 = /^([a-zA-Z]+)$/
+    let firstNameValidity = re2.test(this.state.firstName)
+    if(!firstNameValidity) {
+      this.setState({invalidFirstName:"Enter a valid name"});
+    }
+    let lastNameValidity = re2.test(this.state.lastName);
+    if(!lastNameValidity) {
+      this.setState({invalidLastName:"Enter a valid name"});
+    }
+    return emailValidity&&lastNameValidity&&firstNameValidity;
+  }
 
   handleFormSubmit = (e) => {
     e.preventDefault();
-    const formData = {firstName:this.state.firstName, lastName:this.state.lastName,email:this.state.email};
+    if (this.validateInput()) {
+      const formData = {firstName:this.state.firstName, lastName:this.state.lastName,email:this.state.email};
 
       axios
         .post(`${process.env.REACT_APP_API_URL}/signup`, formData)
@@ -48,10 +72,12 @@ class Subscribe extends React.Component {
         .catch(err => {
           console.log(err.message);
         });
-    console.log(this.state)
+
+    }
   }
 
   render() {
+    console.log(this.state.email)
 
     return (
       <>
@@ -67,7 +93,7 @@ class Subscribe extends React.Component {
             value={this.state.firstName}
             placeholder={"Enter first name"}
             handleChange={this.handleInput}
-          />{" "}
+          />{this.state.invalidFirstName}
           <FormInput
             inputType={"text"}
             title={"Last Name"}
@@ -75,16 +101,16 @@ class Subscribe extends React.Component {
             value={this.state.lastName}
             placeholder={"Enter last name"}
             handleChange={this.handleInput}
-          />{" "}
+          />{this.state.invalidLastName}
 
           <FormInput
-            inputType={"text"}
+            inputType={"email"}
             title={"Email"}
             name={"email"}
             value={this.state.email}
             placeholder={"Enter email"}
             handleChange={this.handleInput}
-          />{" "}
+          />{this.state.invalidEmail}
           {/* Clear the form */}
           <Button
           action={this.handleFormSubmit}
