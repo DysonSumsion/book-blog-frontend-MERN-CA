4597e3deaf26a0dd060d8416fe39b2fef05490d5
Garry <pathakg028@gmail.com>
2019-07-24 14:27:22 +1000

    updated subscribe page validation

--- src/Private.js
+++ src/Private.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import FormAdd from './container/FormAdd';
 import AdminShowPage from './container/AdminShowPage';
 import AdminAddReview from './container/AdminAddReview';
-import Dashboard from './components/Dashboard';
 import ResetPassword from './container/ResetPassword';
 
 class Private extends React.Component {
-
-render() {
-  return (
-    <Switch>
-      <Route path="/auth/adminaddreview" exact render={(props) => {
-        return <FormAdd {...props} />
-      }} />
-      <Route path="/auth/adminshow" exact render={(props) => {
-        return <AdminShowPage {...props} />
-      }} />
-      <Route path="/auth/ForgotPassword" exact component={ForgotPassword}/>
-      <Route path="/auth/ResetPassword" exact component={ResetPassword}/>
-    </Switch>
-  )
-  }
+  
+  render() {
+ return (
+  <Switch>
+   <Route path="/auth/adminaddreview" exact render={(props) => {
+    return <AdminAddReview {...props} />
+   }} />
+   <Route path="/auth/adminshow" exact render={(props) => {
+    return <AdminShowPage {...props} />
+   }} />
+   <Route path="/auth/ResetPassword" exact component={ResetPassword}/>
+  </Switch>
+ )
+ }
 }
 
 export default Private;
