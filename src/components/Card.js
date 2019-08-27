9afa935fa1cd0a7af98d2e2d3490608c9fe9c808
Anna Roszko <roszko.anna@gmail.com>
2019-08-27 10:08:18 +1000

    prettier code clean-up

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,32 @@
 import React from "react";
-import './Card.css';
-import {Link} from 'react-router-dom';
+import "./Card.css";
+import { Link } from "react-router-dom";
 
 function Card(props) {
-
-return (
+  return (
     <div className="card">
       <div className="cardLeft">
-      <img src={props.review.props.children.props.url} alt={props.title}  ></img>
+        <img src={props.review.props.children.props.url} alt={props.title} />
       </div>
       <div className="cardRight">
         <div className="cardRightTop">
           <p className="cardTitle">{props.review.props.children.props.title}</p>
           <p>{props.review.props.children.props.author}</p>
 
-            { props.review.props.children.props.topPick === true && 
-              <span className="topPickDiv">
-                <i className="star icon"></i><p>Top Pick</p>
-              </span>
-            }
-
+          {props.review.props.children.props.topPick === true && (
+            <span className="topPickDiv">
+              <i className="star icon" />
+              <p>Top Pick</p>
+            </span>
+          )}
         </div>
         <div className="cardRightBottom">
-          <a className="outlineButton" href={'/show/' + props.review.props.children.props.title}  >Read More</a>
+          <a
+            className="outlineButton"
+            href={"/show/" + props.review.props.children.props.title}
+          >
+            Read More
+          </a>
         </div>
       </div>
     </div>
